ec4e0d54ee0e637daaa9ebe1e1a08e92940b8b96
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-05-21 10:54:46 +0700

    Add findAll function to PasteContentRepository

--- src/main/java/java/io/scalable/project/backend/repository/PasteContentRepository.java
+++ src/main/java/java/io/scalable/project/backend/repository/PasteContentRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.io.scalable.project.backend.model.PasteContent;
+import java.util.List;
 
 @Repository
 public interface PasteContentRepository extends CrudRepository<PasteContent, Long> {
@@ -11,4 +12,6 @@ public interface PasteContentRepository extends CrudRepository<PasteContent, Lon
     PasteContent findById(long id);
 
     PasteContent findByTitle(String title);
+
+    List<PasteContent> findAll();
 }
