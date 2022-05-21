0fd4db8eca5952dd4a605286a7e93cf3c3fe3acd
TaeXtream <59652919+taextreme@users.noreply.github.com>
2022-05-21 12:21:47 +0700

    Optimize Import

--- src/main/java/java/io/scalable/project/backend/repository/PasteContentRepository.java
+++ src/main/java/java/io/scalable/project/backend/repository/PasteContentRepository.java
@@ -1,6 +1,5 @@
 package java.io.scalable.project.backend.repository;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
